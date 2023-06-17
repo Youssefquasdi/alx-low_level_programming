f77e67fd31098eb087f4acf2a63b7ef62bdedf90
YoussefQuasdi <Quasdi.youssef@gmail.com>
2023-06-16 19:22:46 -0700

    C

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,24 +7,6 @@
  */
 int main(void)
 {
-  int i;
-
-  for (i = 0; i < 10; i++)
-    {
-      printf("%d", i);
-    }
-  putchar('\n');
-  return (0);
-}
-11;rgb:0000/0000/0000#include <stdio.h>
-
-/**
- * main - Prints numbers from 0 to 9.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
 	int i;
 
 	for (i = 0; i < 10; i++)
