2fc00a610396f7100dbd75fafaa3dbc3bd30c770
Youssefquasdi <133615250+Youssefquasdi@users.noreply.github.com>
2023-12-02 00:25:31 +0000

    codding

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,30 @@
-unsigned long int hash_djb2(const unsigned char *str)
-{
-    unsigned long int hash;
-    int c;
+#include "hash_tables.h"
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ * Return: A pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	ht->size = size;
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
